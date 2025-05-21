e9f8a8cc529e4a9215a8c019bba48ccf6dcdd39c
DayoungChoii <young12430@gmail.com>
2025-05-21 21:16:24 +0900

    Feat: Redis 모듈의 캐시를 cacheManager를 추가한다.

--- async-domain/redis/src/main/kotlin/com/redis/config/RedisCacheConfig.kt
+++ async-domain/redis/src/main/kotlin/com/redis/config/RedisCacheConfig.kt
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.RedisSerializer
@@ -14,8 +17,24 @@ import org.springframework.data.redis.serializer.RedisSerializer
 @Configuration
 class RedisCacheConfig {
 
+    @Bean
+    fun cacheManager(redisConnectionFactory: RedisConnectionFactory): CacheManager {
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(redisCacheConfiguration())
+            .build()
+    }
+
+    @Bean
+    fun redisCacheConfiguration(): RedisCacheConfiguration {
+        return RedisCacheConfiguration.defaultCacheConfig()
+            .disableCachingNullValues()
+            .serializeKeysWith(keySerializer)
+            .serializeValuesWith(valueSerializer)
+    }
+
     private val keySerializer
         get() = RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string())
+
     private val valueSerializer
         get() = RedisSerializationContext.SerializationPair.fromSerializer(
             GenericJackson2JsonRedisSerializer(
@@ -29,11 +48,4 @@ class RedisCacheConfig {
             .activateDefaultTyping(
                 BasicPolymorphicTypeValidator.builder().allowIfBaseType(Any::class.java).build(),
                 ObjectMapper.DefaultTyping.NON_FINAL)
-    @Bean
-    fun redisCacheConfiguration(): RedisCacheConfiguration {
-        return RedisCacheConfiguration.defaultCacheConfig()
-            .disableCachingNullValues()
-            .serializeKeysWith(keySerializer)
-            .serializeValuesWith(valueSerializer)
-    }
 }
