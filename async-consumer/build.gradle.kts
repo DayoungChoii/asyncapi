b81dfbe641b28ad738f7a92be2c0aa8f4d90f2d1
DayoungChoii <young12430@gmail.com>
2025-06-09 19:18:51 +0900

    Refactor: 필요없는 의존성 제거

--- async-consumer/build.gradle.kts
+++ async-consumer/build.gradle.kts
@@ -30,7 +30,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("com.mysql:mysql-connector-j")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
