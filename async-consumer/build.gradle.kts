76d1d97dd69a2f5b9bc0846b15b1feaad6e9037b
DayoungChoii <young12430@gmail.com>
2025-07-01 20:10:50 +0900

    Build: consumer 카프카 의존성을 추가한다.

--- async-consumer/build.gradle.kts
+++ async-consumer/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.springframework.kafka:spring-kafka")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
